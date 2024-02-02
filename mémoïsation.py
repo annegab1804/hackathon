1fd92f495ba8e25e6da24fa6fe96df0613a6d498
Anne-Gabrielle Gibeili <anne-gabrielle.gibeili@etu.minesparis.psl.eu>
2024-02-02 11:48:53 +0100

    v2

--- mémoïsation.py
+++ mémoïsation.py
@@ -1,23 +1,23 @@
-V = {0 : [1,3], 1 : [0,2,4], 2: [1,5], 3: [0,4,6], 4: [3,1,5,6], 5: [2,4,8], 6:[3,7], 7: [6,4,8], 8:[7,5]} #matrice modélisant le graphe
+V = { 0 : [1,3], 1 : [0,2,4], 2 : [1,5], 3 : [0,4,6], 4 : [3,1,5,6], 5 : [2,4,8], 6 :[3,7], 7 : [6,4,8], 8 : [7,5] } #matrice modélisant le graphe
 
-D = {"123456780": 0} #dictionnaire donnant la distance minimale d'une configuration
+D = { "123456780" : 0 } #dictionnaire donnant la distance minimale d'une configuration
 
 def distance(M):
-    if M == "123456780":
+    if M == "123456780" :
         return 0
-    else:
-        if M not in D.keys():
-            i=0
-            L=list(M) # on transforme le mot en liste
+    else :
+        if M not in D.keys() :
+            i = 0
+            L = list(M) # on transforme le mot en liste
             while L[i] != "0":
-                i=i+1 #on a la position de notre 0
+                i = i + 1 #on a la position de notre 0
             d = float("inf")
             for v in V[i]:
                 L2 = L.copy()
                 L2[i] , L2[v] = L[v], "0"
                 M2 = "".join(L2)
                 print(M2)
-                try: 
+                try : 
                     dd = distance(M2)
                     if dd< d:
                         d = dd
