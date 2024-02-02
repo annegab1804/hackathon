9dd16b27a21af2f4b46e1b5f4620179516207bad
Louise Philibert <louise.philibert--nicol@etu.minesparis.psl.eu>
2024-02-02 11:44:34 +0100

    corrections

--- v1_resolution.py
+++ v1_resolution.py
@@ -6,7 +6,7 @@ def taquin(m_problem):
     queue = [[[1,2,3],[4,5,6],[7,8,0]]]
     dico[str(queue[0])] = ([],distance)
     while str(m_problem) not in dico.keys() :
-        current_matrix= queue [0]
+        current_matrix = queue [0]
         new_matrices = new_configs (current_matrix)
         queue.pop(0)
         for i in new_matrices :
@@ -15,7 +15,7 @@ def taquin(m_problem):
         for j in new_matrices:
             if str(j) not in dico.keys():
                 path = [current_matrix] + dico[str(current_matrix)][0]
-                dico[str(j)] = (path,dico[str(current_matrix)][1]+1)
+                dico[str(j)] = (path,dico[str(current_matrix)][1] + 1)
     return dico[str(m_problem)]
     
         
@@ -23,23 +23,23 @@ def taquin(m_problem):
 def new_configs(current_mat):
     for i in range (3):
         for j in range (3):
-            if current_mat[i][j]==0:
-                position_0=[i,j]
-    possibilities= []
-    if position_0[0] ==0:
-        possibilities.append ([position_0[0]+1,position_0[1]])
-    if position_0[0] ==1:
-        possibilities.append ([position_0[0]+1,position_0[1]])
-        possibilities.append ([position_0[0]-1,position_0[1]])
-    if position_0[0] ==2:
-        possibilities.append ([position_0[0]-1,position_0[1]])
+            if current_mat[i][j] == 0:
+                position_0 = [i,j]
+    possibilities = []
+    if position_0[0] == 0:
+        possibilities.append ([position_0[0] + 1, position_0[1]])
+    if position_0[0] == 1:
+        possibilities.append ([position_0[0] + 1, position_0[1]])
+        possibilities.append ([position_0[0] - 1, position_0[1]])
+    if position_0[0] == 2:
+        possibilities.append ([position_0[0] - 1, position_0[1]])
     if position_0[1] ==0:
-        possibilities.append ([position_0[0],position_0[1]+1])
+        possibilities.append ([position_0[0],position_0[1] + 1])
     if position_0[1] ==1:
-        possibilities.append ([position_0[0],position_0[1]+1])
-        possibilities.append ([position_0[0],position_0[1]-1])
+        possibilities.append ([position_0[0], position_0[1] + 1])
+        possibilities.append ([position_0[0], position_0[1] - 1])
     if position_0[1] ==2:
-        possibilities.append ([position_0[0],position_0[1]-1])
+        possibilities.append ([position_0[0], position_0[1] - 1])
     new_matrices = []
     for k in possibilities:
         add_matrice = copy.deepcopy(current_mat)
